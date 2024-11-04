5e312ce9f28c574a767c16ba8b758ea60e30879a
bysungjoon <bysungjoon@gmail.com>
2024-11-04 16:50:37 +0900

    baekjoon-2869

---
+++ Math/baekjoon-2869.js
@@ -0,0 +1,34 @@
+var fs = require("fs");
+const { join } = require("path");
+var input = fs
+  .readFileSync("./example.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+/*let input = require('fs').readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);*/
+
+const [A, B, V] = input;
+
+let day = (V - B) / (A - B);
+
+console.log(Math.ceil(day));
+
+// 단순 반복문 사용 시 시간초과
+/*let meter = 0;
+let day = 0;
+
+while (meter <= V) {
+  day++;
+  meter += A;
+  if (meter >= V) {
+    break;
+  }
+  meter -= B;
+}
+
+console.log(day);*/
