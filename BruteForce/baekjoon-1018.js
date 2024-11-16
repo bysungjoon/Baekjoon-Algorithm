ef122e1992fbec867650d5c0273719b8561b4ff5
bysungjoon <bysungjoon@gmail.com>
2024-11-16 22:51:28 +0900

    baekjoon-1018

---
+++ BruteForce/baekjoon-1018.js
@@ -0,0 +1,61 @@
+var fs = require("fs");
+var input = fs.readFileSync("./example.txt").toString().trim().split("\n");
+/*let input = require('fs').readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);*/
+
+const [N, M] = input.shift().split(" ").map(Number);
+let board = input.map((idx) => idx.split(""));
+let answer = [];
+
+const white = [
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+];
+
+const black = [
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+];
+
+function whiteFirst(x, y) {
+  let count = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (board[i + x][j + y] !== white[i][j]) count++;
+    }
+  }
+  return count;
+}
+
+function blackFirst(x, y) {
+  let count = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (board[i + x][j + y] !== black[i][j]) count++;
+    }
+  }
+  return count;
+}
+
+for (let j = 0; j < N - 7; j++) {
+  for (let k = 0; k < M - 7; k++) {
+    answer.push(whiteFirst(j, k));
+    answer.push(blackFirst(j, k));
+  }
+}
+console.log(Math.min(...answer));
