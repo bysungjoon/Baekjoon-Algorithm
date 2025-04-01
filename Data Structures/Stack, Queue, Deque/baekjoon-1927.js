3658b4cfa2e51a7f76e1f9ea732451dcee3adbdd
bysungjoon <bysungjoon@gmail.com>
2025-04-01 17:12:49 +0900

    최소 힙

---
+++ Data Structures/Stack, Queue, Deque/baekjoon-1927.js
@@ -0,0 +1,84 @@
+var fs = require("fs");
+var input = fs
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+/*let input = require('fs').readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);*/
+
+class Minheap {
+  constructor() {
+    this.heap = [];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.heap.length == 0) return 0;
+    if (this.heap.length == 1) return this.heap.pop();
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return min;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[parentIndex] <= this.heap[index]) break;
+      [this.heap[parentIndex], this.heap[index]] = [
+        this.heap[index],
+        this.heap[parentIndex],
+      ];
+      index = parentIndex;
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+    const length = this.heap.length;
+    while (true) {
+      let left = index * 2 + 1;
+      let right = index * 2 + 2;
+      let smallest = index;
+
+      if (left < length && this.heap[left] < this.heap[smallest]) {
+        smallest = left;
+      }
+      if (right < length && this.heap[right] < this.heap[smallest]) {
+        smallest = right;
+      }
+      if (smallest === index) break;
+
+      [this.heap[index], this.heap[smallest]] = [
+        this.heap[smallest],
+        this.heap[index],
+      ];
+      index = smallest;
+    }
+  }
+}
+
+const N = input.shift();
+const heap = new Minheap();
+let answer = [];
+
+input.forEach((value) => {
+  if (value > 0) {
+    heap.push(value);
+  } else {
+    answer.push(heap.pop());
+  }
+});
+
+console.log(answer.join("\n"));
